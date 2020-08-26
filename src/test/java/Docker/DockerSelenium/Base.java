cac7f04bb8b57587e0d90373217ee78a8b96096f
Sinha <priyanka.sinha@inbl2-1h0g3qd.apac.thermo.com>
2020-08-26 23:27:34 +0530

    Fitst Commit

---
+++ src/test/java/Docker/DockerSelenium/Base.java
@@ -0,0 +1,17 @@
+package Docker.DockerSelenium;
+
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class Base {
+	
+	DesiredCapabilities cap=DesiredCapabilities.chrome();
+	
+	//URL url=new URL("url");
+	
+	//WebDriver driver=new RemoteWebDriver(url,cap);
+
+}
